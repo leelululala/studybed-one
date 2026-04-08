6d73e50049bdfb5b891435ff13437b91abe16d81
leeminje <gtyfhvb58462@gmail.com>
2026-04-08 16:36:23 +0900

    get 추가

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -4,10 +4,21 @@ const ApiUtils = {
   //POST 통신을 위한 함수
   sendPost: async (url, data, config = {}) => {
     try {
-      const response = await apiClient.post(url, data, config);
+      const response = await apiClient.post(url, data, { ...config });
       return response.data;
     } catch (error) {
-      console.error(`[API 에러]: ${url}`, error);
+      console.error(`[POST 에러]: ${url}`, error);
+      throw error;
+    }
+  },
+
+  //GET 통신을 위한 함수
+  sendGet: async (url, params, config = {}) => {
+    try {
+      const response = await apiClient.get(url, { params, ...config });
+      return response.data;
+    } catch (error) {
+      console.error(`[GET 에러]: ${url}`, error);
       throw error;
     }
   },
