5c0535785305144eab05915f64c40da347b09b3f
leeminje <gtyfhvb58462@gmail.com>
2026-04-22 20:35:04 +0900

    수정

--- src/features/auth/components/SignUpForm.jsx
+++ src/features/auth/components/SignUpForm.jsx
@@ -7,39 +7,49 @@ import { Switch } from '@/components/ui/switch';
 import AuthCard from './AuthCard';
 import FormField from '@/features/auth/components/FormField';
 import { useSignUp } from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const SignUpForm = () => {
+  const navigate = useNavigate();
   const { mutate: signUp, isPending } = useSignUp();
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     control,
   } = useForm({
     resolver: zodResolver(signUpSchema),
-    mode: 'onBlur',
+    mode: 'onChange',
     defaultValues: { isTwoFa: false, isFido: false },
   });
 
   const onSubmit = (data) => {
     console.log('[signup form] 서버로 보낼 데이터:', data);
+    console.log('is Pending: ', isPending);
     signUp(data);
   };
 
   return (
     <AuthCard
-      title="Sign Up"
-      description="회원가입을 위해 정보를 입력해주세요"
+      title="나만의 워크스페이스"
+      description="이메일로 가입"
       footer={
         <Field className="grid grid-cols-2 gap-2">
           <Button type="button" variant="outline">
             취소
           </Button>
-          <Button type="submit" form="form-signup" disabled={isPending}>
+          <Button
+            type="submit"
+            form="form-signup"
+            disabled={isPending || !isValid}
+          >
             회원가입
           </Button>
         </Field>
       }
+      linkText="Existing User?"
+      linkButtonText="Log in"
+      onLinkClick={() => navigate('/login')}
     >
       <form
         id="form-signup"
@@ -90,13 +100,13 @@ const SignUpForm = () => {
           <Field orientation="horizontal">
             <FieldLabel htmlFor="isFido">생체인증 사용 여부</FieldLabel>
             <Controller
-              name="isFido" 
+              name="isFido"
               control={control}
               render={({ field }) => (
                 <Switch
                   id="isFido"
                   checked={field.value}
-                  onCheckedChange={field.onChange} 
+                  onCheckedChange={field.onChange}
                 />
               )}
             />
